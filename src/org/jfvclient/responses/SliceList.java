7681d716aabfbfc4c090d4da36c43c0b12b0a965
Niklas Rehfeld <niklas.rehfeld@ecs.vuw.ac.nz>
2013-06-13 14:26:10 +1200

    implemented list-slices closes issue #7

---
+++ src/org/jfvclient/responses/SliceList.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package org.jfvclient.responses;
+
+import java.util.ArrayList;
+
+/**
+ * Just a thin wrapper over an ArrayList of Slices.
+ *
+ * @author Niklas Rehfeld
+ *
+ */
+public class SliceList extends ArrayList<Slice>
+{
+
+}
---
+++ tests/org/jfvclient/ListSlicesRRTest.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package org.jfvclient;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This requires us to be able to create and delete slices.
+ *
+ * @author Niklas Rehfeld
+ *
+ */
+public class ListSlicesRRTest
+{
+
+	@Test
+	public void test()
+	{
+
+	}
+
+}
