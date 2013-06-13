177ce94a46810e01382f84d6243c06c1a035286e
Niklas Rehfeld <niklas.rehfeld@ecs.vuw.ac.nz>
2013-06-14 11:01:19 +1200

    updated ListDatapathRRTest to use new JFVRequest constructor.

--- tests/org/jfvclient/ListDatapathRRTest.java
+++ tests/org/jfvclient/ListDatapathRRTest.java
@@ -19,30 +19,33 @@ public class ListDatapathRRTest
 {
 
 	JFVClient c = new JFVClient();
+
 	@Test
 	public void testValidRequest() throws MalformedURLException, IOException
 	{
 		Gson g = TestUtils.getGson();
-		FVRpcRequest ldiRequest = new FVRpcRequest(FVRpcRequest.NoParamType.list_datapaths, "ldp1");
-		String resp = c.send(g,ldiRequest);
+		FVRpcRequest ldiRequest = new FVRpcRequest(
+				FVRpcRequest.NoParamType.list_datapaths);
+		String resp = c.send(g, ldiRequest);
 
 		FVRpcResponse<DataPaths> res = null;
-        Type respType = new TypeToken<FVRpcResponse<DataPaths>>()
-                {
-                }.getType();
-        res = g.fromJson(resp, respType);
+		Type respType = new TypeToken<FVRpcResponse<DataPaths>>()
+		{
+		}.getType();
+		res = g.fromJson(resp, respType);
 
-        assertTrue("Result should not be an error ", !res.isError());
-        DataPaths d = res.getResult();
-//        first dpid shoud be 00::06.
-//        assertEquals(d.get(0), new Dpid(Dpid.toDpid(6)));
-        assertTrue("Should contain DPID 00::05", d.contains(new Dpid(Dpid.toDpid(5))));
-        assertTrue("Should contain DPID 00::06", d.contains(new Dpid(Dpid.toDpid(6))));
-        assertTrue("Should contain DPID 00::07", d.contains(new Dpid(Dpid.toDpid(7))));
-        assertEquals("Unexpected number of DPIDs in results", d.size(), 3);
+		assertTrue("Result should not be an error ", !res.isError());
+		DataPaths d = res.getResult();
+		// first dpid shoud be 00::06.
+		// assertEquals(d.get(0), new Dpid(Dpid.toDpid(6)));
+		assertTrue("Should contain DPID 00::05",
+				d.contains(new Dpid(Dpid.toDpid(5))));
+		assertTrue("Should contain DPID 00::06",
+				d.contains(new Dpid(Dpid.toDpid(6))));
+		assertTrue("Should contain DPID 00::07",
+				d.contains(new Dpid(Dpid.toDpid(7))));
+		assertEquals("Unexpected number of DPIDs in results", d.size(), 3);
 
 	}
-
-
 
 }
