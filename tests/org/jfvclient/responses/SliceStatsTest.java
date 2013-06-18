9cf13d1d7ee6b6d5eebc82ae198ad9388dbf11e2
Nik <niklas.rehfeld@ecs.vuw.ac.nz>
2013-06-18 13:33:51 +1200

    moved SliceStatsTest into proper directory (from src/ into test/

---
+++ tests/org/jfvclient/responses/SliceStatsTest.java
@@ -0,0 +1,28 @@
+package org.jfvclient.responses;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.jfvclient.testing.TestUtils;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class SliceStatsTest
+{
+
+	@Test
+	public void test() throws IOException
+	{
+		String in = TestUtils.readTestInput(SliceStats.class);
+		Gson g = TestUtils.getGson();
+		SliceStats s = g.fromJson(in,SliceStats.class);
+		assertNotNull(s.getRx());
+		assertNotNull(s.getTx());
+		assertNotNull(s.getDrop());
+
+
+	}
+
+}
