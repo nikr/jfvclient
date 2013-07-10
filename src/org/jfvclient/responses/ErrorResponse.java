b8cb55458fbaebd0c738ea2bfc7a339981805217
Niklas Rehfeld <niklas.rehfeld@ecs.vuw.ac.nz>
2013-07-10 15:29:22 +1200

    added docs on what the error codes mean, and added getReason() method that returns string descritions of the error codes.

--- src/org/jfvclient/responses/ErrorResponse.java
+++ src/org/jfvclient/responses/ErrorResponse.java
@@ -17,34 +17,129 @@ package org.jfvclient.responses;
 
 /**
  * ErrorResponse message from FlowVisor.
- * 
+ *
+ * The codes defined by FlowVisor are:
+ * <table>
+ * <tbody>
+ * <tr>
+ * <th>code</th>
+ * <th>message</th>
+ * <th>meaning</th>
+ * </tr>
+ * <tr>
+ * <td>-32700</td>
+ * <td>Parse error</td>
+ * <td>Invalid JSON was received by the server.</td>
+ * </tr>
+ * <tr>
+ * <td>-32600</td>
+ * <td>Invalid Request</td>
+ * <td>The JSON sent is not a valid Request object.</td>
+ * </tr>
+ * <tr>
+ * <td>-32601</td>
+ * <td>Method not found</td>
+ * <td>The method does not exist / is not available.</td>
+ * </tr>
+ * <tr>
+ * <td>-32602</td>
+ * <td>Invalid params</td>
+ * <td>Invalid method parameter(s).</td>
+ * </tr>
+ * <tr>
+ * <td>-32603</td>
+ * <td>Internal error</td>
+ * <td>Internal JSON-RPC error.</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ *
  * @author Niklas Rehfeld
  */
 public class ErrorResponse
 {
 
-    private int code;
-    private String msg;
-    private Object data;
+	/** Invalid JSON was received by the server. */
+	public static final int PARSE_ERROR = -32700;
+	/** The JSON sent is not a valid Request object. */
+	public static final int INVALID_REQUEST = -32600;
+	/** The method does not exist / is not available */
+	public static final int METHOD_NOT_FOUND = -32601;
+	/** Invalid method parameter(s). */
+	public static final int INVALID_PARAMS = -32602;
+	/** Internal JSON-RPC error */
+	public static final int INTERNAL_ERROR = -32603;
 
-    public int getCode()
-    {
-        return code;
-    }
+	private int code;
+	private String msg;
+	private Object data;
 
-    public String getMsg()
-    {
-        return msg;
-    }
+	/**
+	 * Get the error code.
+	 *
+	 * @return the error code.
+	 */
+	public int getCode()
+	{
+		return code;
+	}
 
-    public Object getData()
-    {
-        return data;
-    }
-    
-    @Override
-    public String toString()
-    {
-        return "Code: " + code + ", Message: " + msg;
-    }
+	/**
+	 * gets the reason or meaning of the error, as defined in the table above.
+	 *
+	 * @return the meaning of the error code.
+	 */
+	public String getReason()
+	{
+		String reason;
+		switch (code)
+		{
+		case PARSE_ERROR:
+			reason = "Invalid JSON was received by the server.";
+			break;
+		case METHOD_NOT_FOUND:
+			reason = "The method does not exist / is not available.";
+			break;
+		case INVALID_REQUEST:
+			reason = "The JSON sent is not a valid Request object.";
+			break;
+		case INVALID_PARAMS:
+			reason = "Invalid method parameter(s)";
+			break;
+		case INTERNAL_ERROR:
+			reason = "Internal JSON-RPC error.";
+			break;
+		default:
+			reason = "Unknown";
+			break;
+		}
+
+		return reason;
+
+	}
+
+	/**
+	 *
+	 * @return The error message.
+	 */
+	public String getMsg()
+	{
+		return msg;
+	}
+
+	/**
+	 *
+	 * @return extra data that relates to the error.
+	 */
+	public Object getData()
+	{
+		return data;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Code: " + code + ", Message: " + msg
+				+ (data != null ? ", Data: " + data : "");
+	}
 }
