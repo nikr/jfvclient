efbb6734182b80b051fe6ec82c1bcf307c6d0a8c
Niklas Rehfeld <niklas.rehfeld@ecs.vuw.ac.nz>
2013-07-10 14:13:58 +1200

    modified Dpid class to include a constructor from a long. Modified Dpid.equals() to return true if both of the dpids are any of 'all', 'any' or 'all_dpids'. also added unit tests for Dpid class. This completes most of #30, except that the constructor is not yet used anywhere.

--- src/org/jfvclient/data/Dpid.java
+++ src/org/jfvclient/data/Dpid.java
@@ -24,79 +24,130 @@ import java.awt.font.NumericShaper;
 public class Dpid
 {
 
-    private String dpid;
-    private final String matchRegex = "([a-fA-F0-9]{2}:){7}[a-fA-F0-9]{2}";
+	private String dpid;
+	private final String matchRegex = "([a-fA-F0-9]{2}:){7}[a-fA-F0-9]{2}";
 
-    public Dpid(String dpid)
-    {
-        this.dpid = dpid;
-    }
+	/**
+	 * Creates a new DPID given a DPID string. The string supplied must be either
+	 * <ul>
+	 * <li/> of the form hh:hh:hh:hh:hh:hh:hh:hh
+	 * <li/> "any", "all" or "all_dpids"
+	 * </ul>
+	 * @param dpid
+	 */
+	public Dpid(String dpid)
+	{
+		this.dpid = dpid;
+	}
 
-    public String getDpid()
-    {
-        return dpid;
-    }
+	/**
+	 * creates a new DPID from a value.
+	 *
+	 * note that as a long is signed (2's complement), the dpid
+	 * ff:...:ff actually corresponds to -1L.
+	 *
+	 * @param dpid the long value of a DPID.
+	 */
+	public Dpid(long dpid)
+	{
+		String raw = Long.toHexString(dpid);
+		int padding = 16 - raw.length();
+		for (int i = 0; i < padding; i++)
+		{
+			raw = "0" + raw;
+		}
+		this.dpid = "";
+		int j = 0;
+		for (; j < 14; j+= 2)
+		{
+			this.dpid += raw.substring(j,  j+2) + ":";
+		}
+		this.dpid += raw.substring(j,  j+2);
+	}
 
-    /**
-     * This is a convenience method for simple dpids in test setups, such as using Mininet.
-     * In these cases the DPIDs will be something like 00:00:...:00:01 for the first switch etc.
-     * This converts the device number into a simple DPID like that. It only works if the device number is &lteq; FF.
-     *
-     * <emph>Should not be used in production code.</emph>
-     *
-     * @param dnum
-     * @return
-     */
-    public static String toDpid(int dnum)
-    {
-    	String finalbits = Integer.toHexString(dnum);
-    	if (finalbits.length() < 2)
-    	{
-    		finalbits = "0" + finalbits;
-    	}
+	public String getDpid()
+	{
+		return dpid;
+	}
 
-    	return "00:00:00:00:00:00:00:" + finalbits;
-    }
 
-    public boolean isValid()
-    {
-        if (dpid.matches(matchRegex))
-        {
-            return true;
-        }
-        if (dpid.equalsIgnoreCase("any") || dpid.equalsIgnoreCase("all") || dpid.equalsIgnoreCase("ALL_DPIDS"))
-        {
-            return true;
-        }
-        return false;
+	/**
+	 * This is a convenience method for simple dpids in test setups, such as
+	 * using Mininet. In these cases the DPIDs will be something like
+	 * 00:00:...:00:01 for the first switch etc. This converts the device number
+	 * into a simple DPID like that. It only works if the device number is
+	 * &lteq; FF.
+	 *
+	 * <emph>Should not be used in production code.</emph>
+	 *
+	 * @param dnum
+	 * @return
+	 * @deprecated(use constructor Dpid(long) instead.)
+	 */
+	@Deprecated
+	public static String toDpid(int dnum)
+	{
+		String finalbits = Integer.toHexString(dnum);
+		if (finalbits.length() < 2)
+		{
+			finalbits = "0" + finalbits;
+		}
 
-    }
+		return "00:00:00:00:00:00:00:" + finalbits;
+	}
 
-    /**
-     * returns true iff o is a Dpid AND the dpid strings match.
-     */
-    @Override
-    public boolean equals(Object o)
-    {
-    	Dpid other;
-    	if (!(o instanceof Dpid))
-    		return false;
-    	else
-    	{other = (Dpid) o;}
+	public boolean isValid()
+	{
+		if (dpid.matches(matchRegex))
+		{
+			return true;
+		}
+		if (dpid.equalsIgnoreCase("any") || dpid.equalsIgnoreCase("all")
+				|| dpid.equalsIgnoreCase("ALL_DPIDS"))
+		{
+			return true;
+		}
+		return false;
 
-    	return (other.getDpid().equalsIgnoreCase(dpid));
+	}
 
-    }
+	/**
+	 * returns true iff o is a Dpid AND the dpid strings are the same,
+	 * OR both of them are one of "any", "all" or "all_dpids".
+	 */
+	@Override
+	public boolean equals(Object o)
+	{
+		Dpid other;
+		if (!(o instanceof Dpid))
+			return false;
+		else
+		{
+			other = (Dpid) o;
+		}
+		String otherString = other.getDpid();
+		if(otherString.equalsIgnoreCase(dpid))
+		{
+			return true;
+		}
+		if (otherString.equalsIgnoreCase("all") || otherString.equalsIgnoreCase("any") || otherString.equalsIgnoreCase("all_dpids"))
+		{
+			return (dpid.equalsIgnoreCase("all") || dpid.equalsIgnoreCase("any") || dpid.equalsIgnoreCase("all_dpids"));
+		}
+		return false;
 
-    @Override
-    public int hashCode()
-    {
-    	return dpid.hashCode();
-    }
 
-    @Override
-    public String toString()
-    {
-    	return dpid;
-    }
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return dpid.hashCode();
+	}
+
+	@Override
+	public String toString()
+	{
+		return dpid;
+	}
 }
---
+++ tests/org/jfvclient/data/DpidTest.java
@@ -0,0 +1,91 @@
+/**
+ *
+ */
+package org.jfvclient.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Niklas Rehfeld
+ *
+ */
+public class DpidTest
+{
+
+
+
+	/**
+	 * Test method for {@link org.jfvclient.data.Dpid#Dpid(long)}.
+	 */
+	@Test
+	public void testDpidLong()
+	{
+		Dpid d = new Dpid(1L);
+		assertTrue(d.isValid());
+
+		d = new Dpid(12L);
+		assertTrue(d.isValid());
+
+		d = new Dpid(123L);
+		assertTrue(d.isValid());
+
+		d = new Dpid(1234L);
+		assertTrue(d.isValid());
+
+		d = new Dpid(3234567890123456789L);
+		assertTrue(d.isValid());
+
+		d= new Dpid(Long.MAX_VALUE);
+		assertTrue(d.isValid());
+
+		d = new Dpid(Long.MIN_VALUE);
+		assertTrue(d.isValid());
+
+		d = new Dpid(-1L);
+		assertTrue(d.isValid());
+	}
+
+
+	/**
+	 * Test method for {@link org.jfvclient.data.Dpid#isValid()}.
+	 */
+	@Test
+	public void testIsValid()
+	{
+		Dpid d = new Dpid("bla");
+		assertFalse(d.isValid());
+
+		d = new Dpid("alll");
+		assertFalse(d.isValid());
+
+		d= new Dpid("all");
+		assertTrue(d.isValid());
+
+		d= new Dpid("00:00:00:00:00:00:11:11");
+		assertTrue(d.isValid());
+
+	}
+
+	/**
+	 * Test method for {@link org.jfvclient.data.Dpid#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject()
+	{
+		Dpid d = new Dpid(1);
+		Dpid e = new Dpid("00:00:00:00:00:00:00:01");
+		assertTrue(d.equals(e));
+
+		d = new Dpid("all");
+		assertFalse(d.equals(e));
+
+		d = new Dpid("all");
+		e = new Dpid("any");
+		assertTrue(d.equals(e));
+
+
+	}
+
+}
